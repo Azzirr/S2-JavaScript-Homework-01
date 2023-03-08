b1f5a6a8146ade19adf6bf15d5b40b42b5b4827b
Arkadiusz Sliwowski <arek.sliwowski37@gmail.com>
2023-03-08 22:26:57 +0100

    First task

---
+++ 01.js
@@ -0,0 +1,7 @@
+function rotateByKElements(k){
+    let array = [1, 2, 3, 4, 5, 6];
+    let rotate = array.splice(0, k);
+    array.push(...rotate);
+    return array;
+}
+console.log(rotateByKElements(3));
